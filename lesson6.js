88d33a8dcb721e71e579594d1c0e475976964a8c
Jason Almenas <i.striderno9@icloud.com>
2016-06-30 19:17:39 -0400

    practicing code

---
+++ lesson6.js
@@ -0,0 +1,48 @@
+// console.log("hello world");
+// alert("hello world");
+
+
+// document.getElementById("result").innerHTML = "hello world";
+
+
+//datatypes:
+//strings -> words
+//numbers -> 0, 1.5 -2039
+//booleans true or false
+//undefined
+//array -> [ , , ]
+//object -> { , , }
+
+// var fruit = "banana";
+
+// var fruits = ["banana", "apple", "watermelon"];
+// console.log(fruits[2]);
+
+// var fruit = {
+// 	name: "banana",
+// 	color: "yellow"
+// }
+
+// console.log(fruit.color);
+// console.log(fruit["color"]);
+
+// var age = 23;
+
+// if (age >= 21) {
+// 	alert("welcome!");
+// } else {
+// 	alert("go to welche's grape jusice site instead");
+// }
+
+
+
+var johnny = 11;
+
+if (johnny > 10) {
+	alert("Johnny is 11 and that is greater than ten!");
+} else {
+	alert("Johnny is less than ten!");
+}
+
+
+
